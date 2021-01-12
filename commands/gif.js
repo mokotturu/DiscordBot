4e6a37879a5665e8b966b901fd05dba2c0e3708f
mokotturu <monishkotturu@gmail.com>
2021-01-11 23:31:52 -0500

    Initial commit

---
+++ commands/gif.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+    name: 'gif',
+    description: 'GIFs from Giphy. Pronounced GIF, not GIF.',
+    async execute(msg, args) {
+        let keywords;
+        if (args.length > 1) {
+            keywords = args.join("+");
+        } else {
+            keywords = "cat";
+        }
+        let url = `https://api.giphy.com/v1/gifs/search?q=${keywords}&rating=g&api_key=${process.env.GIPHY_API_KEY}`;
+        let res = await fetch(url);
+        let json = await res.json();
+        const index = Math.floor(Math.random() * json.data.length);
+        let result = json.data[index] ? json.data[index].url : "No gif was found with your search parameters.";
+        let sent = msg.channel.send(result);
+        (await sent).react('⬆');
+        (await sent).react('⬇');
+    },
+};
---
+++ commands/hi.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'hi',
+    description: 'Not hi.',
+    execute(msg, args) {
+        msg.reply('not hi.');
+    },
+};
